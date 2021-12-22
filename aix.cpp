9af58e3da99b9d76ab3b6cf6d3928d0bfcab654b
Mahmoud Osman (MLT) <60033959+Mahmoud7Osman@users.noreply.github.com>
2021-12-22 20:00:26 +0200

    Finished The Development of AIXcrypt
    Nothing, but this is probably the last update in this repository, Use it well.

--- aix.cpp
+++ aix.cpp
@@ -8,8 +8,28 @@
 
 using namespace std;
 
+const char* help=
+"AixCrypt: A Steganography-Like Technique Performed By Doing A Very Simple Calculation on Your Data With Another File, Resulting in a Key Used For Later Production Of Your Exact Data.\n"
+"       Usage:   aix  [filename]  [keygen|produce]  -f [File]  -k [Key File]\n\n"
+"               First Argument: [filename], used to calculate a key (keygen) or produce data (produce).\n"
+"                                Caution: If Your using produce on a [filename] that contain data, all those data will be lost.\n\n"
+"		Second Argument:  keygen, keygen is used to calculate key for an exsisting [filename], and output the key into [Key File]\n"
+"			         produce, produce is used to produce data into [filename], by calculating The same file [File] Used in keygen, with an exsisting key.\n\n"
+"		3rd Argument:    [file], [File], Used to calculate Or Produce data (keygen or produce) to/with the keyfile, (Easy, keygen need to write key into the Keyfile, produce need to read the keyfile to produce actual data)\n\n"
+"		4rd Argument:    [Keyfile], [Keyfile] is used to read/write key, keygen need to write a new key, produce need to read then calculate, easy.\n\n"
+"	example:\n"
+"		$ cd AixCrypt/learn\n"
+"		$ cat README.md\n"
+"		  ...\n"
+"		$ aix produce message -f hello.png -k my_key.txt\n"
+"		$ cat message\n"
+"		  ...\n"
+"	So Easy To Use, see the example, try it yourself in AixCrypt/learn\n";
 
 int main(int argc, char** argv){
+
+	if (argc<6||*(argv[1]+1)=='h') return fprintf(stderr, help);
+
 	int   	   opt;
 	int    produce;
 	int        key;
@@ -25,7 +45,6 @@ int main(int argc, char** argv){
 
 	while ((opt=getopt(argc, argv, ":f:k:"))!=-1){
 		switch(opt){
-
 			case 'f':
 				arguments[2]=argv[4];
 				break;
--- install.sh
+++ install.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-
+echo
+sleep 1
+printf "[ProTip] It's Better to Run this script as root, in order to execute this program by typing only aix into the cmd line without the path\n"
+echo
+sleep 1
 build(){
 	printf "Building Binaries..\n"
 	mkdir ~/.aix 2>/dev/null
@@ -19,6 +23,7 @@ g++ aix.cpp -o aix
 
 if [ $(whoami) == "root" ];then
   cp aix /bin/aix
+  printf "Succeed, You can now run this program by typing aix in your terminal\n"
 else
   printf "You Can Copy ./aix to /bin to use it without full path (Or Any directory you Prefer presented in the PATH Variable)\n"
 fi
